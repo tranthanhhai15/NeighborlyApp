24185c158857f629a7de7df652bb97f2ae63f008
tranthanhhai15 <haitt.dev15@gmail.com>
2022-04-11 03:20:51 +0700

    update eventhub trigger

--- NeighborlyAPI/eventHubTrigger/__init__.py
+++ NeighborlyAPI/eventHubTrigger/__init__.py
@@ -2,14 +2,7 @@ import json
 import logging
 import azure.functions as func
 
-
 def main(event: func.EventGridEvent):
-
-	logging.info('Function triggered to process a message: ', event.get_body())
-    logging.info('  EnqueuedTimeUtc =', event.enqueued_time)
-    logging.info('  SequenceNumber =', event.sequence_number)
-    logging.info('  Offset =', event.offset)
-
     result = json.dumps({
         'id': event.id,
         'data': event.get_json(),
@@ -20,6 +13,4 @@ def main(event: func.EventGridEvent):
 
 
     logging.info('Python EventGrid trigger processed an event: %s', result)
-
-
 
